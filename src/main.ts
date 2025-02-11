06289115f4e7494da539963438fd91cd182f306d
Florian Van Belleghem <fvanbelleghem@iclouod.com>
2025-02-11 15:15:48 +0100

    feat: add cloth physics

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,134 @@
 import * as THREE from "three";
+import RAPIER from "@dimforge/rapier3d";
+import { EXRLoader } from "three/examples/jsm/Addons.js";
 
-const GRAVITY = -9.81;
+const SEGMENTS = 10;
+const MASS = 0.5;
+const VERTEX_DISTANCE = 1 / SEGMENTS;
 
 const canvas = document.getElementById("canvas")!;
 
+const gravity = { x: 0, y: 0, z: 10 };
+const world = new RAPIER.World(gravity);
+
 const scene = new THREE.Scene();
-const clock = new THREE.Clock();
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  canvas.clientWidth / canvas.clientHeight
+);
 const plane = createPlane();
+const bodies = createBodies();
+const joints = createJoints();
+
+const background = await loadEnvironment("rosendal_plains_1_2k.exr");
+background.mapping = THREE.EquirectangularReflectionMapping;
+scene.background = new THREE.Color("grey");
+scene.environment = background;
 
 scene.add(plane);
 camera.position.z = 5;
+
 update();
+
+function update() {
+  requestAnimationFrame(update);
+  updatePhysics();
+  updateGeometry();
+  render();
+}
+
+function updatePhysics() {
+  world.step();
+}
+
+function updateGeometry() {
+  const positions = plane.geometry.attributes.position;
+
+  for (let i = 0; i < positions.count; i++) {
+    const newPosition = bodies[i].translation();
+    positions.setXYZ(i, newPosition.x, newPosition.y, newPosition.z);
+  }
+
+  positions.needsUpdate = true;
+  plane.geometry.computeVertexNormals();
+}
 
 function render() {
   renderer.render(scene, camera);
 }
 
-function animate(delta: number) {
-  const speeds = plane.geometry.getAttribute("speed");
-  const positions = plane.geometry.getAttribute("position");
-
-  for (let i = 0; i < positions.count; i++) {
-    const speedY = speeds.array[i * speeds.itemSize];
-    const positionY = positions.array[i * positions.itemSize + 1];
-
-    const nextSpeedY = speedY + GRAVITY * delta;
-    const nextPositionY = positionY + speedY * delta + GRAVITY * Math.pow(delta, 2) / 2;
-
-    speeds.array[i * speeds.itemSize] = nextSpeedY;
-    positions.array[i * positions.itemSize + 1] = nextPositionY;
-  }
-  
-  speeds.needsUpdate = true;
-  positions.needsUpdate = true;
-}
-
-function update() {
-  const delta = clock.getDelta();
-
-  requestAnimationFrame(update)
-  animate(delta);
-  render();
-}
-
 function createPlane() {
-  const material = new THREE.MeshBasicMaterial({ wireframe: true });
-  const geometry = new THREE.PlaneGeometry(1, 1, 10, 10);
+  const material = new THREE.MeshStandardMaterial({
+    wireframe: false,
+    metalness: 0.1,
+    roughness: 0.1,
+  });
+  const geometry = new THREE.PlaneGeometry(1, 1, SEGMENTS, SEGMENTS);
+  const mesh = new THREE.Mesh(geometry, material);
 
-  const speed = new Float32Array(geometry.getAttribute("position").count);
-  geometry.setAttribute("speed", new THREE.BufferAttribute(speed, 1));
-
-  return new THREE.Mesh(geometry, material);
+  return mesh;
 }
 
+function createBodies() {
+  const bodies: RAPIER.RigidBody[] = [];
+  const vertex = new THREE.Vector3();
+
+  const isFixed = (i: number, j: number) =>
+    i % SEGMENTS === 0 || j % SEGMENTS === 0;
+
+  for (let i = 0; i < SEGMENTS + 1; i++) {
+    for (let j = 0; j < SEGMENTS + 1; j++) {
+      const index = i * (SEGMENTS + 1) + j;
+
+      vertex.fromBufferAttribute(plane.geometry.attributes.position, index);
+      vertex.applyMatrix4(plane.matrixWorld);
+
+      const method = isFixed(i, j) ? "fixed" : "dynamic";
+      const bodyDesc = RAPIER.RigidBodyDesc[method]()
+        .setAdditionalMass(MASS)
+        .setTranslation(...vertex.toArray());
+
+      const body = world.createRigidBody(bodyDesc);
+      bodies.push(body);
+    }
+  }
+
+  return bodies;
+}
+
+function createJoints() {
+  const joints: RAPIER.ImpulseJoint[] = [];
+
+  const connect = (bodyA: RAPIER.RigidBody, bodyB: RAPIER.RigidBody) => {
+    const origin = { x: 0, y: 0, z: 0 };
+    const jointParams = RAPIER.JointData.spring(
+      VERTEX_DISTANCE,
+      100,
+      10,
+      origin,
+      origin
+    );
+
+    const joint = world.createImpulseJoint(jointParams, bodyA, bodyB, true);
+    joints.push(joint)
+  };
+
+  for (let i = 0; i < SEGMENTS + 1; i++) {
+    for (let j = 0; j < SEGMENTS + 1; j++) {
+      const index = i * (SEGMENTS + 1) + j;
+
+      if (j < SEGMENTS) connect(bodies[index], bodies[index + 1]);
+      if (i < SEGMENTS) connect(bodies[index], bodies[index + SEGMENTS + 1]);
+    }
+  }
+
+  return joints;
+}
+
+async function loadEnvironment(url: string) {
+  const loader = new EXRLoader();
+  return new Promise<THREE.DataTexture>((resolve, reject) => {
+    loader.load(url, (data) => resolve(data), undefined, reject);
+  });
+}
