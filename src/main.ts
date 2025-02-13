bf53756e19e0ad50582874a280a4fb3e5ec890a3
Florian Van Belleghem <fvanbelleghem@iclouod.com>
2025-02-13 11:25:57 +0100

    feat: add velvet texture

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,30 @@ import RAPIER from "@dimforge/rapier3d";
 import { EXRLoader } from "three/examples/jsm/Addons.js";
 
 const ENVIRONMENT_PATH = "/cloth-button/rosendal_plains_1_2k.exr";
-const TEXTURE_PATH = "/cloth-button/text.png";
+const ALBEDO_TEXTURE_PATH = "/cloth-button/text.png";
+const NORMAL_TEXTURE_PATH = "/cloth-button/velvet_normal.webp";
 
 const WIDTH = 2;
 const HEIGHT = 1;
 const ASPECT_RATIO = WIDTH / HEIGHT;
-const DETAIL = 7;
+const DETAIL = 10;
 const WIDTH_SEGMENTS = DETAIL;
 const HEIGHT_SEGMENTS = Math.floor(WIDTH_SEGMENTS * ASPECT_RATIO);
-const RELAX_FACTOR = 1.1;
+const RELAX_FACTOR = 1;
 const HORIZONTAL_VERTEX_DISTANCE = (RELAX_FACTOR * WIDTH) / WIDTH_SEGMENTS;
 const VERTICAL_VERTEX_DISTANCE = (RELAX_FACTOR * HEIGHT) / HEIGHT_SEGMENTS;
+const DIAGONAL_VERTEX_DISTANCE = Math.sqrt(
+  Math.pow(HORIZONTAL_VERTEX_DISTANCE, 2) +
+    Math.pow(VERTICAL_VERTEX_DISTANCE, 2)
+);
 const MASS = 1;
-const PRE_ITERATIONS = 200;
+const PRE_ITERATIONS = 2000;
 
 const canvas = document.getElementById("canvas")!;
 
-const gravity = { x: 0, y: 0, z: 70 };
+const gravity = { x: 0, y: 0, z: 100 };
 const world = new RAPIER.World(gravity);
+world.timestep = 1 / 30;
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
@@ -79,16 +85,26 @@ function render() {
 }
 
 async function createPlane() {
-  const map = await loadTexture(TEXTURE_PATH);
+  const map = await loadTexture(ALBEDO_TEXTURE_PATH);
   map.offset.set(-0.5, 0);
   map.repeat.set(WIDTH, HEIGHT);
   map.wrapS = map.wrapT = THREE.ClampToEdgeWrapping;
 
-  const material = new THREE.MeshStandardMaterial({
+  const normalMap = await loadTexture(NORMAL_TEXTURE_PATH);
+  const factor = 0.5;
+  normalMap.wrapS = normalMap.wrapT = THREE.RepeatWrapping;
+  normalMap.repeat.set(WIDTH * factor, HEIGHT * factor);
+
+  const material = new THREE.MeshPhysicalMaterial({
     wireframe: false,
-    metalness: 0.1,
-    roughness: 0.1,
+    color: "salmon",
     map,
+    normalMap,
+    normalScale: new THREE.Vector2(0.35, 0.35),
+    metalness: 0,
+    roughness: 0.8,
+    sheen: 0.5,
+    sheenRoughness: 0.6,
   });
   const geometry = new THREE.PlaneGeometry(
     WIDTH,
@@ -158,6 +174,18 @@ function createJoints() {
           bodies[index + WIDTH_SEGMENTS + 1],
           VERTICAL_VERTEX_DISTANCE
         );
+      if (i < HEIGHT_SEGMENTS && j < WIDTH_SEGMENTS)
+        connect(
+          bodies[index],
+          bodies[index + WIDTH_SEGMENTS + 2],
+          DIAGONAL_VERTEX_DISTANCE
+        );
+      if (i > 0 && j < WIDTH_SEGMENTS)
+        connect(
+          bodies[index],
+          bodies[index - WIDTH_SEGMENTS],
+          DIAGONAL_VERTEX_DISTANCE
+        );
     }
   }
 
@@ -178,10 +206,12 @@ async function loadTexture(url: string) {
   });
 }
 
-const colliderDesc = RAPIER.ColliderDesc.ball(1).setTranslation(0, 0, 10);
+const colliderDesc = RAPIER.ColliderDesc.ball(1)
+colliderDesc.setEnabled(false);
 const mouseCollider = world.createCollider(colliderDesc);
 
 canvas.addEventListener("mousemove", (event) => {
+  mouseCollider.setEnabled(true);
   const mousePosition = { x: 0, y: 0 };
   mousePosition.x = (event.clientX / canvas.clientWidth) * 2 - 1;
   mousePosition.y = -(event.clientY / canvas.clientHeight) * 2 + 1;
